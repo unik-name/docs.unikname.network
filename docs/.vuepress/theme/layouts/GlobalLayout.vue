395327c908a213e742d1b53f7a4569d816d7bc7a
laurentlourenco <laurent@lourenco.pro>
2019-12-10 21:15:19 +0100

    add default global layout

---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,23 @@
+<!-- themePath/layouts/GlobalLayout.vue -->
+<template>
+  <div id="global-layout">
+    <component :is="layout"/>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    layout () {
+      if (this.$page.path) {
+        if (this.$frontmatter.layout) {
+          // You can also check whether layout exists first as the default global layout does.
+          return this.$frontmatter.layout
+        }
+        return 'Layout'
+      }
+      return 'NotFound'
+    }
+  }
+}
+</script>
