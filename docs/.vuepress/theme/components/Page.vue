4fa693a1fd55094f1a28af32bbcb610858fc92d8
laurentlourenco <laurent@lourenco.pro>
2019-12-10 13:24:41 +0100

    eject theme for later customoization

---
+++ docs/.vuepress/theme/components/Page.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <Content class="theme-default-content" />
+
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+
+    <slot name="bottom" />
+
+    <Footer class="pagefooter" />
+    
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+
+export default {
+  components: { PageEdit, PageNav },
+  props: ['sidebarItems']
+}
+</script>
+
+<style lang="stylus">
+@require '../styles/wrapper.styl'
+
+.page
+  padding-bottom 2rem
+  display block
+
+.pagefooter
+  padding 2rem 0rem 0rem 2rem
+</style>
