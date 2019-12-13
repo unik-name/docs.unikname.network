eff3728f25ccd104bd7658e139400c01818efa7f
laurent <laurent@spacelephant.org>
2019-12-13 21:18:20 +0100

    kpi tuning

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', '2']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src='matomo.js'; s.parentNode.insertBefore(g,s);
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
       })();
     `]
   ],
