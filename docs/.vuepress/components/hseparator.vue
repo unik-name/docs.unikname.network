417b15d61cfd791228a94491f69d2f654f15411a
laurent <laurent@spacelephant.org>
2020-11-18 19:29:58 +0100

    component: activating hseparator.vue

---
+++ docs/.vuepress/components/hseparator.vue
@@ -0,0 +1,19 @@
+<template>
+<img src="/logo.png" class="hseparator" :style="{ 'padding-top': y, 'padding-bottom': y }"/>
+</template>
+
+<script>
+export default {
+    props: { 
+        y: {type: String, default: "40px"},  
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.hseparator
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+</style>
+
