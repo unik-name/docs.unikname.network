9d6ea248902004461b471e8cf8d05fe7b91d26b1
laurent <laurent@spacelephant.org>
2020-11-18 21:27:35 +0100

    components: adding hbox and unikname

---
+++ docs/.vuepress/components/hbox.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="hbox"><slot></slot></div>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus" scoped>
+.hbox
+    padding-left: 25px
+
+    th
+        text-align left
+        vertical-align top
+        font-weight 400
+        border-left unset
+        border-right unset
+    tr
+        text-align left
+        vertical-align top
+        font-weight 400
+    
+    td
+        border-left unset
+        border-right unset   
+    
+    tr:nth-child(even) {background-color: #fff;}
+
+</style>
+
